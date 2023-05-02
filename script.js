01ee4c546108f45a16ae7a5624592a79f6ac2d12
justintate <justinpjtate1@gmail.com>
2023-05-02 13:39:55 +0100

    Added carousel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myCarouselElement = document.querySelector('#carouselExample')
+const myCarouselElement = document.querySelector('#carouselMain')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
